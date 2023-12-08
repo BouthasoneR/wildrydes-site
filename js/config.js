c1cc9325312f43cb2a8a2501674b7d5e403502ca
BouthasoneR <121906515+BouthasoneR@users.noreply.github.com>
2023-12-08 14:02:29 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-2_rC5iJFPNg',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '4aqujf8u5rhs0dloqgkus7g59c',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             'us-east-2'          // e.g. us-east-2
+        userPoolId:         'us-east-1_IdiV4Wh6y',         
+        userPoolClientId:   'eqslrou33q65h2gos5mbtpgkv',         =
+        region:             'us-east-1'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          'https://gjg5k15df5.execute-api.us-east-2.amazonaws.com/production'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
